bbb9cef8f89b900c5db7ffa39c0821bbf7d931a7
tanwlanyue <tanwlanyue@gmail.com>
2021-09-17 12:08:28 +0800

    并查集

--- src/LC0547/Solution.java
+++ src/LC0547/Solution.java
@@ -2,6 +2,7 @@ package LC0547;
 
 /**
  * 并查集 也可用dfs
+ * 
  * @author zhanglei211 on 2021/9/17.
  */
 class Solution {
@@ -30,14 +31,52 @@ class Solution {
         return count;
     }
 
+    /**
+     * 合并两个节点，两个节点的parent关联在一起
+     * 
+     * @param index1
+     * @param index2
+     */
     public void union(int index1, int index2) {
         parent[find(index1)] = find(index2);
     }
 
+    /**
+     * 更新index的parent,并返回
+     * 
+     * @param index
+     * @return
+     */
     public int find(int index) {
         if (parent[index] != index) {
             parent[index] = find(parent[index]);
         }
         return parent[index];
     }
+
+    // public int findCircleNum(int[][] isConnected) {
+    // int n = isConnected.length;
+    // int count = 0;
+    // boolean[] visited = new boolean[n];
+    // for (int i = 0; i < n; i++) {
+    // for (int j = 0; j < n; j++) {
+    // if (isConnected[i][j] == 1 && !visited[i]) {
+    // dfs(isConnected, i, visited);
+    // count++;
+    // }
+    // }
+    // }
+    // return count;
+    // }
+    //
+    // private void dfs(int[][] isConnected, int i, boolean[] visited) {
+    // int n = isConnected.length;
+    // for (int j = 0; j < n; j++) {
+    // if (isConnected[i][j] == 1 && !visited[j]) {
+    // visited[j] = true;
+    // dfs(isConnected, j, visited);
+    // }
+    // }
+    // }
+
 }
