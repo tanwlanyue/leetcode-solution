6c7f2e88b8397ebe9fc5d6f02be4c35629ba29c7
tanwlanyue <tanwlanyue@gmail.com>
2021-10-14 21:06:38 +0800

    最长公共子序列

---
+++ src/LC0583/Solution.java
@@ -0,0 +1,29 @@
+package LC0583;
+
+/**
+ * 可以转换为求两个字符串的最长公共子序列问题
+ * 
+ * @author zhanglei211 on 2021/10/14.
+ */
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int len1 = word1.length();
+        int len2 = word2.length();
+        int[][] dp = new int[len1 + 1][len2 + 1];
+
+        for (int i = 0; i <= len1; i++) {
+            for (int j = 0; j <= len2; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                } else {
+                    if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                        dp[i][j] = dp[i - 1][j - 1] + 1;
+                    } else {
+                        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    }
+                }
+            }
+        }
+        return len1 + len2 - dp[len1][len2] * 2;
+    }
+}
---
+++ src/LC1143/Solution.java
@@ -0,0 +1,27 @@
+package LC1143;
+
+/**
+ * 最长公共子序列
+ * @author zhanglei211 on 2021/10/14.
+ */
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int len1 = text1.length();
+        int len2 = text2.length();
+        int[][] dp = new int[len1 + 1][len2 + 1];
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                } else {
+                    if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                        dp[i][j] = dp[i - 1][j - 1] + 1;
+                    } else {
+                        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    }
+                }
+            }
+        }
+        return dp[len1][len2];
+    }
+}
