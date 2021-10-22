fc9808cecfed5e5d0ab8a2e5651248e662223f65
tanwlanyue <tanwlanyue@gmail.com>
2021-10-22 15:10:11 +0800

    寻找两个正序数组的中位数

--- src/LC0004/Solution.java
+++ src/LC0004/Solution.java
@@ -16,28 +16,24 @@ class Solution {
     }
 
     private double getTopK(int[] nums1, int[] nums2, int k) {
-        int len1 = nums1.length;
-        int len2 = nums2.length;
-        int index1 = 0;
-        int index2 = 0;
+        int index1 = 0, index2 = 0, len1 = nums1.length, len2 = nums2.length;
         while (true) {
             if (index1 == len1) {
                 return nums2[index2 + k - 1];
-            }
-            if (index2 == len2) {
+            } else if (index2 == len2) {
                 return nums1[index1 + k - 1];
-            }
-            if (k == 1) {
+            } else if (k == 1) {
                 return Math.min(nums1[index1], nums2[index2]);
-            }
-            int newIndex1 = Math.min(index1 + k / 2, len1) - 1;
-            int newIndex2 = Math.min(index2 + k / 2, len2) - 1;
-            if (nums1[newIndex1] < nums2[newIndex2]) {
-                k -= newIndex1 - index1 + 1;
-                index1 = newIndex1 + 1;
             } else {
-                k -= newIndex2 - index2 + 1;
-                index2 = newIndex2 + 1;
+                int newIndex1 = Math.min(index1 + k / 2, len1) - 1;
+                int newIndex2 = Math.min(index2 + k / 2, len2) - 1;
+                if (nums1[newIndex1] < nums2[newIndex2]) {
+                    k = k - (newIndex1 - index1 + 1);
+                    index1 = newIndex1 + 1;
+                } else {
+                    k = k - (newIndex2 - index2 + 1);
+                    index2 = newIndex2 + 1;
+                }
             }
         }
     }
