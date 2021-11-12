dd4037bc46a9ef05dae9740d9b2e0b62c576b35d
tanwlanyue <tanwlanyue@gmail.com>
2021-11-12 11:51:50 +0800

    队列实现栈

---
+++ src/LC0225/MyStack.java
@@ -0,0 +1,38 @@
+package LC0225;
+
+import java.util.LinkedList;
+
+/**
+ * @author zhanglei211 on 2021/11/10.
+ */
+class MyStack {
+    LinkedList<Integer> queue1;
+    LinkedList<Integer> queue2;
+
+    public MyStack() {
+        this.queue1 = new LinkedList<>();
+        this.queue2 = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        queue2.offer(x);
+        while (!queue1.isEmpty()) {
+            queue2.offer(queue1.poll());
+        }
+        LinkedList<Integer> temp = queue1;
+        queue1 = queue2;
+        queue2 = temp;
+    }
+
+    public int pop() {
+        return queue1.poll();
+    }
+
+    public int top() {
+        return queue1.peek();
+    }
+
+    public boolean empty() {
+        return queue1.isEmpty();
+    }
+}
