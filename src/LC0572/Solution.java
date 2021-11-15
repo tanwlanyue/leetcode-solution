b8598e60f31f9602ec1d3c0be1a2af87c524f2c6
tanwlanyue <tanwlanyue@gmail.com>
2021-11-15 20:17:34 +0800

    另一棵树的子树

---
+++ src/LC0572/Solution.java
@@ -0,0 +1,30 @@
+package LC0572;
+
+/**
+ * 另一棵树的子树
+ * @author zhanglei211 on 2021/11/15.
+ */
+class Solution {
+
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        return dfs(root, subRoot);
+    }
+
+    private boolean dfs(TreeNode root, TreeNode subRoot) {
+        if (root == null) {
+            return false;
+        }
+        return check(root, subRoot) || dfs(root.left, subRoot) || dfs(root.right, subRoot);
+    }
+
+    private boolean check(TreeNode root, TreeNode subRoot) {
+        if (root == null && subRoot == null) {
+            return true;
+        }
+        if (root == null || subRoot == null || root.val != subRoot.val) {
+            return false;
+        }
+        return check(root.left, subRoot.left) && check(root.right, subRoot.right);
+    }
+
+}
