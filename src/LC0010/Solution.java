f79f6ab13222d6f47406d786328f1b35c5cedbc1
tanwlanyue <tanwlanyue@gmail.com>
2021-11-16 11:01:52 +0800

    正则表达式

---
+++ src/LC0010/Solution.java
@@ -0,0 +1,31 @@
+package LC0010;
+
+/**
+ * @author zhanglei211 on 2021/11/15.
+ */
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int sLen = s.length();
+        int pLen = p.length();
+        boolean[][] dp = new boolean[sLen + 1][pLen + 1];
+        dp[0][0] = true;
+        for (int i = 2; i <= pLen; i = i + 2) {
+            if (p.charAt(i - 1) == '*') {
+                dp[0][i] = dp[0][i - 2];
+            }
+        }
+        for (int i = 1; i <= sLen; i++) {
+            for (int j = 1; j <= pLen; j++) {
+                if (p.charAt(j - 1) == '.' || s.charAt(i - 1) == p.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i][j - 2];
+                    if (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.') {
+                        dp[i][j] = dp[i][j] || dp[i - 1][j];
+                    }
+                }
+            }
+        }
+        return dp[sLen][pLen];
+    }
+}
