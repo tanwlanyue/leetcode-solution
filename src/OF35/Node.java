4aca1e85a1a83c12a7c573deb74c8a388fbe6c9d
tanwlanyue <tanwlanyue@gmail.com>
2021-11-23 14:00:57 +0800

    update

---
+++ src/OF35/Node.java
@@ -0,0 +1,16 @@
+package OF35;
+
+/**
+ * @author zhanglei211 on 2021/11/22.
+ */
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
