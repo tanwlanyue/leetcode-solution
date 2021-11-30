91a34196cfe14e69fd47f44120dc14df6b48ea0d
tanwlanyue <tanwlanyue@gmail.com>
2021-11-30 14:43:06 +0800

    去除重复字母

--- src/LC0316/Solution.java
+++ src/LC0316/Solution.java
@@ -1,23 +1,46 @@
 package LC0316;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
- * TODO
+ * 去除重复字母
+ * 
  * @author zhanglei211 on 2021/11/19.
  */
 class Solution {
+
     public String removeDuplicateLetters(String s) {
-        char[] chars = s.toCharArray();
-        ArrayList<Integer>[] indexTable = new ArrayList[26];
-        Arrays.fill(indexTable, new ArrayList<Integer>());
-        for (int i = 0; i < chars.length; i++) {
-            indexTable[chars[i] - 'a'].add(i);
+        boolean[] used = new boolean[26];
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
         }
-        for (int i = 0; i < chars.length; i++) {
-            indexTable[chars[i] - 'a'].add(i);
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            // 已经在栈中的不要插入
+            if (!used[c - 'a']) {
+                while (!stack.isEmpty() && stack.peek() > c) {
+                    // 后面还有相同的字符，可以弹出
+                    Character peek = stack.peek();
+                    if (count[peek - 'a'] > 0) {
+                        used[peek - 'a'] = false;
+                        stack.pop();
+                    } else {
+                        break;
+                    }
+                }
+                used[c - 'a'] = true;
+                stack.push(c);
+            }
+            count[c - 'a'] -= 1;
         }
-        return "";
+        StringBuilder builder = new StringBuilder();
+        while (!stack.isEmpty()) {
+            builder.insert(0, stack.pop());
+        }
+        return builder.toString();
     }
 }
