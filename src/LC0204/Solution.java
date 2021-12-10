336bbeb3fca56d7d5ac10beb8d7d16fbed84124f
tanwlanyue <tanwlanyue@gmail.com>
2021-12-10 15:00:15 +0800

    TODO

--- src/LC0204/Solution.java
+++ src/LC0204/Solution.java
@@ -1,7 +1,11 @@
 package LC0204;
 
 /**
+ * TODO
  * @author zhanglei211 on 2021/12/8.
  */
-public class Solution {
+class Solution {
+    public int countPrimes(int n) {
+        return 0;
+    }
 }
